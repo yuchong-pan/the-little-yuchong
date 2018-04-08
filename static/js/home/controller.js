12b3edbbd301aab6234c6443cc23a2b0f3ae41a0
yuchong-pan <panyuchong@gmail.com>
2018-04-08 16:25:44 -0700

    Added Vue controller for homepage

---
+++ static/js/home/controller.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#main",
+    data: {
+        questions: [{
+            question: 'Are you Yuchong Pan?',
+            answer: 'Yes. Just call me Yuchong.'
+        }, {
+            question: 'Where are you studying, Yuchong?',
+            answer: 'The University of British Columbia.'
+        }, {
+            question: 'So, do you like Vancouver?',
+            answer: 'Um, too much rain.'
+        }]
+    }
+});

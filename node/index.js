9f9b7a17995d56181a172ada9721d7969a0dba62
yuchong-pan <panyuchong@gmail.com>
2018-04-08 15:50:02 -0700

    Added HTML & CSS of homepage

--- node/index.js
+++ node/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile('./src/pages/home/index.html');
+    res.sendFile('./src/pages/home/index.html', { 'root' : __dirname });
 });
+
+app.use('/node_modules', express.static('node_modules'));
 
 app.listen(8080);

4582b800b7fdd579705af771360de1429ed86894
yuchong-pan <panyuchong@gmail.com>
2018-04-08 17:05:06 -0700

    Added typewriter effect in Vue controller for homepage

--- static/js/home/controller.js
+++ static/js/home/controller.js
@@ -1,7 +1,10 @@
-new Vue({
+let vm = new Vue({
     el: "#main",
     data: {
-        questions: [{
+        speed: 50,
+        pause: 300,
+        bigPause: 1000,
+        all: [{
             question: 'Are you Yuchong Pan?',
             answer: 'Yes. Just call me Yuchong.'
         }, {
@@ -10,6 +13,37 @@ new Vue({
         }, {
             question: 'So, do you like Vancouver?',
             answer: 'Um, too much rain.'
-        }]
+        }],
+        questions: []
+    },
+    mounted: function() {
+        this.loadQuestions(0);
+    },
+    methods: {
+        loadQuestions: function(i) {
+            let that = this;
+            if (i < that.all.length) {
+                that.questions.push({ question: '', answer: '' });
+                let typeAnswer = function() {
+                    that.type(that.all[i].answer, 0, i, 'answer', function() {
+                        that.loadQuestions(i + 1);
+                    }, that.bigPause);
+                };
+                that.type(that.all[i].question, 0, i, 'question', typeAnswer, that.pause);
+            }
+        },
+        type: function(text, i, kth, which, next, pause) {
+            let that = this;
+            if (i < text.length) {
+                that.questions[kth][which] += text.charAt(i);
+                setTimeout(function() {
+                    that.type(text, i + 1, kth, which, next, pause);
+                }, that.speed);
+            } else {
+                setTimeout(function() {
+                    next();
+                }, pause);
+            }
+        }
     }
 });
